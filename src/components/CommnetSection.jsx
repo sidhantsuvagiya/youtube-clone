1081223d53e8e3efb82be16576b586231b6f25b9
sidhantsuvagiya <66551578+sidhantsuvagiya@users.noreply.github.com>
2024-01-31 19:30:04 +0530

    fixed error

--- src/components/CommnetSection.jsx
+++ src/components/CommnetSection.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import YoutubeComment from "./YoutubeComment";
 
 const CommnetSection = ({ videoId, commentCount }) => {
